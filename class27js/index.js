f01bf9c0f0a7793a113afdf3a472c3a65486cba3
Artiom <trukhanartiom@gmail.com>
2021-06-25 11:29:02 +0300

    My HomeWork

---
+++ class27js/index.js
@@ -0,0 +1,15 @@
+"" + 1 + 0 == "10" //String
+"" - 1 + 0 == 1 //number
+true + false == 1 //number
+6 / "3" == 2 //number
+"2" * "3" == 6 //number 
+4 + 5 + px == "9function pxa(a){return a instanceof oxa}" //string
+"$" + 4 + 5 == "$45" //string
+"4" - 2 == 2 //number
+"4px" - 2 == NaN  //undefined
+7 / 0 == Infinity  //undefined
+" -9\n" + 5 == " -9\n5" //string
+" -9\n" - 5 == -14 //number
+null + 1 == 1 //number
+undefined + 1 == NaN //undefined
+null == "\n0\n" == false //number
